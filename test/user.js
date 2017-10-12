265ec3f2d97fa5d22def70ae559a738fa9594427
Thomas Charuel <thomascharuel@hotmail.fr>
2017-10-12 15:54:03 +0200

    test file changes

--- test/user.js
+++ test/user.js
@@ -1,25 +1,23 @@
 var should = require('should');
-var user = require('../src/user');
+var user = require('../lib/user');
 
 describe('user', function(){
-  it('save properly', function(){
+  it('save properly', function(done){
     
     user.save("ok", "pwd", function(err){
       should.not.exist(err);
       done();
     });
-    
-    done();
   });
 
-  it('doesn\'t save because missing parameters', function(){
+  it('doesn\'t save because missing parameters', function(done){
     user.save("only name", function(err){
       should.exist(err);
       done();
     });
   });
 
-  it('get', function(){
+  it('get', function(done){
     // ...
     
     done();
